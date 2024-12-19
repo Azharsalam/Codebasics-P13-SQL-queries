9908e80dcb146b4f1dcd707422aca090fa296232
Azharsalam <azharsalam137@gmail.com>
2024-12-19 11:21:44 +0530

    Update Business Requirment 6.sql

--- Business Requirment 6.sql
+++ Business Requirment 6.sql
@@ -1,14 +1,4 @@
-SELECT
-    dc.city_name,
-    MONTHNAME(fps.month) AS month,
-    SUM(fps.total_passengers) AS total_passengers,
-    (SUM(fps.repeat_passengers) / SUM(fps.total_passengers)) * 100 AS monthly_repeat_passenger_rate,
-    city_totals.city_repeat_passenger_rate
-FROM
-    fact_passenger_summary AS fps
-JOIN dim_city AS dc
-    ON fps.city_id = dc.city_id
-JOIN (
+WITH CityTotals AS (
     SELECT
         city_id,
         SUM(total_passengers) AS total_passengers,
@@ -18,9 +8,20 @@ JOIN (
         fact_passenger_summary
     GROUP BY
         city_id
-) AS city_totals
-    ON fps.city_id = city_totals.city_id
+)
+SELECT
+    dc.city_name,
+    MONTHNAME(fps.month) AS month,
+    SUM(fps.total_passengers) AS total_passengers,
+    ROUND((SUM(fps.repeat_passengers) / SUM(fps.total_passengers)) * 100, 2) AS monthly_repeat_passenger_rate,
+    ROUND(ct.city_repeat_passenger_rate, 2) AS city_repeat_passenger_rate
+FROM
+    fact_passenger_summary AS fps
+JOIN dim_city AS dc
+    ON fps.city_id = dc.city_id
+JOIN CityTotals AS ct
+    ON fps.city_id = ct.city_id
 GROUP BY
-    dc.city_name, MONTHNAME(fps.month), city_totals.city_repeat_passenger_rate
+    dc.city_name, MONTHNAME(fps.month), ct.city_repeat_passenger_rate
 ORDER BY
     dc.city_name, MONTHNAME(fps.month);
